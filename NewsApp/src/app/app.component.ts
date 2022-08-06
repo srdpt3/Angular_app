4abdc5cd03594a355d3124bf5ac018dea8b857a9
Dustin Yang <srdpt3@gmail.com>
2022-08-06 18:41:59 -0400

    update

--- NewsApp/src/app/app.component.ts
+++ NewsApp/src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { NewsService } from './service/news.service';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'NewsApp';
+  public sources: any = [];
+  public articles: any = [];
+  selectedNewsChannel: string = "Top 10 Trending News!";
+
+  @ViewChild(MatSidenav) sideNav!: MatSidenav
+  constructor(private observer: BreakpointObserver,
+    private cdr: ChangeDetectorRef, private newsApi: NewsService) {
+
+  }
+
+  ngOnInit(): void {
+    this.newsApi.initArticles().subscribe((res: any) => {
+      console.log(res);
+      this.articles = res.articles;
+    })
+    this.newsApi.initSources().subscribe((res: any) => {
+      console.log(res);
+      this.sources = res.sources;
+    })
+
+
+  }
+
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:787px)']).subscribe((res) => {
+      if (res?.matches) {
+        this.sideNav.mode = "over"
+        this.sideNav.close();
+      } else {
+        this.sideNav.mode = "side"
+        this.sideNav.open();
+      }
+    });
+    this.cdr.detectChanges();
+  }
+
+  searchSource(source: any) {
+    this.newsApi.getArticlesByID(source.id)
+      .subscribe((res: any) => {
+        this.selectedNewsChannel = source.name
+        this.articles = res.articles;
+      })
+  }
+
+
 }
---
+++ NewsApp/src/app/service/news.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+  api_key = '990a914665a3451e8b666a1cb7fb1e44';
+  constructor(private http: HttpClient) { }
+
+  initSources() {
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
+  }
+
+
+  initArticles() {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
+  }
+
+  getArticlesByID(source: String) {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  }
+}
